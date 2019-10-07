26f5cbae89db38195cb012f869d9342c72eead27
Rúben Filipe <rubenfilipe.net@gmail.com>
2019-10-07 01:09:35 -0300

    initial commit

---
+++ Relogio/README.md
@@ -0,0 +1,4 @@
+# Relogio-Javascript
+Relógio feito com HTML,CSS e JAVASCRIPT.
+
+Tudo incluido no arquivo .html
---
+++ smartDetect.js
@@ -0,0 +1,4 @@
+if(screen.width <=620){
+    alert("Esta página não foi otimizada para dispositivos móveis."+"\n"+"Clique em OK para continuar")
+}
+

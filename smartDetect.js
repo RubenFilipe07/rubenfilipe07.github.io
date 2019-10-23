4d489943f0be7912c78058a083a00924ef5b5760
Rúben Filipe <rubenfilipe.net@gmail.com>
2019-10-22 23:47:25 -0300

    Aprimoramento do css no mobile

--- smartDetect.js
+++ smartDetect.js
@@ -1,4 +1,9 @@
-if(screen.width <=620){
-    alert("Esta página não foi otimizada para dispositivos móveis."+"\n"+"Clique em OK para continuar")
+var url = window.location.href
+
+if(url == "https://rubenfilipe07.github.io/" && screen.width <=620){
+document.getElementById("Css").href = "mobile.css";
 }
 
+if(screen.width <=620 && url!="https://rubenfilipe07.github.io/"){
+ alert("Esta página não foi otimizada para dispositivos móveis."+"\n"+"Clique em OK para continuar")   
+}

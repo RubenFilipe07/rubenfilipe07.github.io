b84aad5274d48e2c73c8a7e3b08c8722afbbc77f
Rúben Filipe <rubenfilipe.net@gmail.com>
2019-10-24 00:55:00 -0300

    Adição do css responsivo

--- smartDetect.js
+++ smartDetect.js
@@ -1,9 +1,16 @@
 var url = window.location.href
+var inicial  = "https://rubenfilipe07.github.io/"
+var xilofone = "https://rubenfilipe07.github.io/Xilofone/index.html"
+var mobile = false
 
-if(url == "https://rubenfilipe07.github.io/" && screen.width <=620){
-document.getElementById("Css").href = "mobile.css";
+if(screen.width <=620){
+mobile = true; 
 }
 
-if(screen.width <=620 && url!="https://rubenfilipe07.github.io/"){
+if(url == inicial && mobile==true){
+document.getElementById("css").href = "mobile.css"
+}
+
+if(url!=inicial && url!=xilofone && mobile==true){
  alert("Esta página não foi otimizada para dispositivos móveis."+"\n"+"Clique em OK para continuar")   
 }
